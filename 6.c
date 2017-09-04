52017660f23451bc5821d1b4c4ef1dff1317cc4e
RAJKARTHI <31534375+RAJKARTHI@users.noreply.github.com>
2017-09-04 19:53:56 +0530

    Update 6.c

--- 6.c
+++ 6.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
-void main()
+ main()
 {
-    int low, high, i, flag;
-    printf("Enter two num(intervals): ");
-    scanf("%d %d", &low, &high);
-
-    printf("Prime num between %d and %d are: ", low, high);
-
-    while (low < high)
+   int low, high, i, flag;
+   printf("Enter two num(intervals): ");
+   scanf("%d %d", &low, &high);
+   printf("Prime num between %d and %d are: ", low, high);
+     while (low < high)
     {
-        flag = 0;
-
-        for(i = 2; i <= low/2; ++i)
+     flag = 0;
+         for(i = 2; i <= low/2; ++i)
         {
-            if(low % i == 0)
-            {
-                flag = 1;
-                break;
+         if(low % i == 0)
+       {
+          flag = 1;
+           break;
             }
         }
